d1c96615e7a271884921a5b78ccccb8548f1b753
Anton Makiievskyi <iammckiev@gmail.com>
2020-03-26 19:31:19 +0200

    fixed logdir

--- TREX_train.py
+++ TREX_train.py
@@ -46,7 +46,7 @@ parser.add_argument('--start_level', type=int, default=0)
 parser.add_argument('--test_worker_interval', type=int, default=0)
 parser.add_argument('--reward_model_path', default='reward_model', help="name and location for learned model params, e.g. ./learned_models/breakout.params")
 
-LOG_DIR = 'TREX_LOG_' +str(env_name) + '_numlvl=' + str(num_levels)
+LOG_DIR = 'TREX_LOG_' + str(args.env_name) + '_numlvl=' + str(args.num_levels)
 args = parser.parse_args()
 
 test_worker_interval = args.test_worker_interval
