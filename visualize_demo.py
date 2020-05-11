45d0943ef3065c054a1367b4aee701c729274735
lzil <liang.zhou.18@ucl.ac.uk>
2020-05-11 20:23:30 +0100

    consistent renaming, rm video log

--- visualize_demo.py
+++ visualize_demo.py
@@ -8,12 +8,14 @@ import cv2
 
 from helpers.utils import get_id
 
-vid_folder = 'videos/demos'
-os.makedirs(vid_folder, exists_ok=True)
+vid_folder = 'videos'
+os.makedirs(vid_folder, exist_ok=True)
 
-# send all the output from cv2 to a log file
-with open('log_visualize_demo.log', 'a') as f:
-    sys.stdout = f
+# # send all the output from cv2 to a log file
+# os.makedirs('logs', exist_ok=True)
+# f = open('logs/log_visualize_demo.log', 'a')
+# sys.stdout = f
+# sys.stderr = f
 
 # only argument is demo path
 parser = argparse.ArgumentParser()
@@ -27,11 +29,15 @@ with open(path, 'rb') as f:
 demo_id = get_id(path)
 observations = demo['observations']
 
+video_path = os.path.join(vid_folder, f'demo_{demo_id}.mp4')
+if os.path.isfile(video_path):
+    os.remove(video_path)
+
 # set up video writer
 videodims = (64, 64)
 fourcc = cv2.VideoWriter_fourcc(*'avc1')
 # fourcc = cv2.VideoWriter_fourcc('H', '2', '6', '4')
-video = cv2.VideoWriter(os.path.join(vid_folder, f'demo_{demo_id}.mp4', fourcc, 40, videodims))
+video = cv2.VideoWriter(video_path, fourcc, 40, videodims)
 
 # produce video
 for i in range(0, len(observations)):
@@ -41,3 +47,4 @@ for i in range(0, len(observations)):
 
 video.release()
 cv2.destroyAllWindows()
+# f.close()
